dac938610c743f856e3932aed187fa8c78cfdfa2
beketamas <tamas.beke0921@gmail.com>
2023-05-19 08:04:49 +0200

    k

---
+++ NyelvvizsgaFeladat/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace NyelvvizsgaFeladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vizsgak> lista = new List<Vizsgak>();
+
+            StreamReader str = new StreamReader("sikeres.csv");
+            StreamReader str2 = new StreamReader("sikertelen.csv");
+            str.ReadLine();
+            while (!str.EndOfStream)
+            {
+                lista.Add(new Vizsgak(str.ReadLine(),true));
+            }
+            str.Close();
+            str2.ReadLine();
+            while (!str2.EndOfStream)
+            {
+                lista.Add(new Vizsgak(str2.ReadLine()));
+                
+            }
+            str2.Close();
+
+            //2. feldata
+            Console.WriteLine("2. feladat:");
+            var listaRendezett = lista.OrderByDescending(x => x.Osszes).TakeWhile((x,i) => i != 3).ToList();
+            listaRendezett.ForEach(x => Console.WriteLine(x.Nyelv));
+
+            //3. feladat
+            Console.WriteLine("3. feladat:");
+            Console.WriteLine("Adjon meg egy évszámot: ");
+            string evszam = Console.ReadLine();
+
+            //4. feladat
+            long osszeg = 0;
+            var sikertelenLista = lista.Where(x => x.SikeresVagyNem == 'n');
+            
+            foreach (var item in sikertelenLista)
+            {
+                osszeg += item.Osszes;
+            }
+            var max = lista.MaxBy(x => (x.Osszes / osszeg) * 100);
+
+            //5. feladat
+            Console.WriteLine("5. feladat:");
+            int szam = Convert.ToInt32(evszam)- 2008;
+            List<string> nullaVizsgazo = new List<string>();
+            StreamReader str3 = new StreamReader("sikeres.csv");
+            StreamReader str4 = new StreamReader("sikertelen.csv");
+            str3.ReadLine();
+            while (!str3.EndOfStream)
+            {
+                string[] tomb = str3.ReadLine().Split(";");
+                if (tomb[szam] == "0")
+                {
+                    nullaVizsgazo.Add(tomb[0]);
+                }
+            }
+            str3.Close();
+            str4.ReadLine();
+            while (!str4.EndOfStream)
+            {
+                string[] tomb = str4.ReadLine().Split(";");
+                if (tomb[szam] == "0")
+                {
+                    nullaVizsgazo.Add(tomb[0]);
+                }
+
+            }
+            str4.Close();
+            if (nullaVizsgazo.Count == 0)
+            {
+                Console.WriteLine("Minden nyelvből volt vizsgázó");
+            }
+            else 
+            {
+                nullaVizsgazo.ForEach(x => Console.WriteLine(x));
+                
+            }
+
+            //6. feladat
+            File.WriteAllLines("osszesites.csv", lista.Select(x => $"{x.Nyelv};{x.Osszes};{(x.Osszes / osszeg) * 100}"));
+
+
+
+        }
+    }
+}
---
+++ NyelvvizsgaFeladat/Vizsgak.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NyelvvizsgaFeladat
+{
+    internal class Vizsgak
+    {
+        string nyelv;
+        int elsoEv;
+        int masodikEv;
+        int harmadikEv;
+        int negyedikEv;
+        int otodikEv;
+        int hatodikEv;
+        int hetedikEv;
+        int nyolcadikEv;
+        int kilencedikEv;
+        int tizedikEv;
+        char sikeresVagyNem;
+
+        public Vizsgak(string sor, bool sikeres)
+        {
+            var mezok = sor.Split(";");
+            this.nyelv = mezok[0];
+            this.elsoEv = int.Parse(mezok[1]);
+            this.masodikEv = int.Parse(mezok[2]);
+            this.harmadikEv = int.Parse(mezok[3]);
+            this.negyedikEv = int.Parse(mezok[4]);
+            this.otodikEv = int.Parse(mezok[5]);
+            this.hatodikEv = int.Parse(mezok[6]);
+            this.hetedikEv = int.Parse(mezok[7]);
+            this.nyolcadikEv = int.Parse(mezok[8]);
+            this.kilencedikEv = int.Parse(mezok[9]);
+            this.tizedikEv = int.Parse(mezok[10]);
+            this.sikeresVagyNem = 'i';
+        }
+        public Vizsgak(string sor)
+        {
+            var mezok = sor.Split(";");
+            this.nyelv = mezok[0];
+            this.elsoEv = int.Parse(mezok[1]);
+            this.masodikEv = int.Parse(mezok[2]);
+            this.harmadikEv = int.Parse(mezok[3]);
+            this.negyedikEv = int.Parse(mezok[4]);
+            this.otodikEv = int.Parse(mezok[5]);
+            this.hatodikEv = int.Parse(mezok[6]);
+            this.hetedikEv = int.Parse(mezok[7]);
+            this.nyolcadikEv = int.Parse(mezok[8]);
+            this.kilencedikEv = int.Parse(mezok[9]);
+            this.tizedikEv = int.Parse(mezok[10]);
+            this.sikeresVagyNem = 'n';
+        }
+
+
+        public string Nyelv { get => nyelv;}
+        public int ElsoEv { get => elsoEv;}
+        public int MasodikEv { get => masodikEv;}
+        public int HarmadikEv { get => harmadikEv;}
+        public int NegyedikEv { get => negyedikEv;}
+        public int OtodikEv { get => otodikEv;}
+        public int HatodikEv { get => hatodikEv;}
+        public int HetedikEv { get => hetedikEv;}
+        public int NyolcadikEv { get => nyolcadikEv;}
+        public int KilencedikEv { get => kilencedikEv;}
+        public int TizedikEv { get => tizedikEv; }
+        public long Osszes { get => elsoEv + masodikEv + harmadikEv + negyedikEv + otodikEv + hatodikEv + hetedikEv + nyolcadikEv + kilencedikEv + TizedikEv; }
+        public char SikeresVagyNem { get => sikeresVagyNem;}
+        
+    }
+}
